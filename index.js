d609165c33457457b07921b1419eb001553a9443
tunnckoCore <mameto_100@mail.bg>
2014-12-30 10:20:56 +0200

    add module pattern, apply code style

--- index.js
+++ index.js
@@ -1,51 +1,52 @@
-var isNode = typeof process !== 'undefined' && process.versions && !!process.versions.node;
-var isBrowser = typeof window !== 'undefined';
+/**
+ * j140 <https://github.com/tunnckoCore/j140>
+ *
+ * Copyright (c) 2014 Charlike Mike Reagent, contributors.
+ * Released under the MIT license.
+ */
 
-if (isNode && !isBrowser) {
-  module.exports = jedExport;
-} else {
-  this.jed = jedExport;
-}
-
-function jedExport(str, options, done) {
-  if (typeof done == 'boolean' && done == true) return jedFn(str, options);
-
-  var raw, output;
-  try {
-    raw = jedFn(str);
-    output = raw(options);
-    if (typeof done == 'function') return done(null, output);
-    else return output;
-  } catch (err) {
-    if (typeof done == 'function') return done(err, null);
-    else return err;
+(function(global, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(factory());
+  } else if (typeof exports === 'object') {
+    module.exports = factory();
+  } else {
+    global.j140 = factory();
   }
-}
+})(this, function() {
+  'use strict';
 
-function jedFn(
-  a, // the string source from which the template is compiled
-  b  // the default `with` context of the template (optional)
-){
-  return function(
-    c, // the object called as `this` in the template
-    d  // the `with` context of this template call (optional)
-  ){
-    return a.replace(
-      /#{([^}]*)}/g, // a regexp that finds the interpolated code: "#{<code>}"
-      function(
-        a, // not used, only positional
-        e  // the code matched by the interpolation
-      ){
-        return Function(
-          "x",
-          "with(x)return " + e // the result of the interpolated code
-        ).call(
-          c,    // pass the data object as `this`, with
-          d     // the most
-          || b  // specific
-          || {} // context.
-        )
+  function jedExport(str, options, done) {
+    if (typeof done === 'boolean' && done === true) {
+      return jedFn(str, options);
+    }
+
+    var raw = '';
+    var output = '';
+
+    try {
+      raw = jedFn(str);
+      output = raw(options);
+    } catch (err) {
+      if (typeof done === 'function') {
+        return done(err, null);
       }
-    )
+      return err;
+    }
+
+    if (typeof done === 'function') {
+      return done(null, output);
+    }
+    return output;
   }
-}
+
+  function jedFn(a, b) {
+    return function(c, d) {
+      return a.replace(/#{([^}]*)}/g, function(a, e) {
+        return new Function('x', 'with(x) return ' + e).call(c, d || b || {});
+      });
+    }
+  }
+
+  return jedExport;
+});
--- test.js
+++ test.js
@@ -1,32 +1,47 @@
-var jed = require('./index');
+/**
+ * j140 <https://github.com/tunnckoCore/j140>
+ *
+ * Copyright (c) 2014 Charlike Mike Reagent, contributors.
+ * Released under the MIT license.
+ */
 
-describe('j140', function () {
-  it('should support locals (sync)', function (done) {
-    var res = jed("Test #{this.state || 'pass'}", {state: 'sync'});
-    res.should.equal('Test sync');
-    done()
+'use strict';
+
+var assert = require('assert');
+var j140 = require('./index');
+
+describe('j140:', function() {
+  it('should support locals (sync)', function(done) {
+    var res = j140('Test #{this.state || \'pass\'}', {state: 'sync'});
+    assert.strictEqual(res, 'Test sync');
+    done();
   });
-  it('should support locals (async)', function (done) {
-    jed("Test #{this.state || 'pass'}", {state: 'async'}, function (err, res) {
-      if (err) return done(err);
-      res.should.equal('Test async');
-      done()
-    });
+  it('should support locals (async)', function(done) {
+    j140('Test #{this.state || \'pass\'}', {state: 'async'}, handler);
+
+    function handler(err, res) {
+      if (err) {
+        return done(err);
+      }
+
+      assert.strictEqual(res, 'Test async');
+      done();
+    }
   });
-  it('should be default value if not locals defined', function (done) {
-    var res = jed("Test #{this.state || 'pass'}");
-    res.should.equal('Test pass');
-    done()
+  it('should be default value if not locals defined', function(done) {
+    var res = j140('Test #{this.state || \'pass\'}');
+    assert.strictEqual(res, 'Test pass');
+    done();
   });
-  it('should return function if 3rd argument is "true"', function (done) {
-    var res = jed("Test #{this.state || 'fn'}", null, true);
-    res.should.be.type('function');
-    done()
+  it('should return function if 3rd argument is "true"', function(done) {
+    var res = j140('Test #{this.state || \'fn\'}', null, true);
+    assert.strictEqual(typeof res, 'function');
+    done();
   });
-  it('should returned function get locals and return template', function (done) {
-    var fn = jed("Test #{this.state || 'fn'}", null, true);
+  it('should return function get locals and return template', function(done) {
+    var fn = j140('Test #{this.state || \'fn\'}', null, true);
     var res = fn({state: 'locals, pass'});
-    res.should.equal('Test locals, pass');
-    done()
+    assert.strictEqual(res, 'Test locals, pass');
+    done();
   });
 });
